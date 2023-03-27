e0c9fbe49eb0eeb3facae3c4e5585a74ce6a573b
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-03-27 12:27:27 +0300

    NetworkManager

--- HW3.4Quotes/Services/NetworkManger.swift
+++ HW3.4Quotes/Services/NetworkManger.swift
@@ -7,3 +7,42 @@
 
 import Foundation
 import Alamofire
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchDataWitchAlamofire(for url: String, completion: @escaping(Result<[Quotes], AFError>) -> Void) {
+        AF.request(url)
+            .validate()
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    let quotes = Quotes.getQuotes(from: value)
+                    completion(.success(quotes))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
+
+class ImageManager {
+    
+    static let shared = ImageManager()
+    
+    private init() {}
+    
+    func fetchImage(from url: String, completion: @escaping(Data) -> Void) {
+        AF.request(url).validate().responseData { data in
+            switch data.result {
+            case .success(let data):
+                completion(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
