650850304938ebff414e9ed1f8028aea3a61e4a0
Maksim Gurkov <maksim.gurkov@gmail.com>
2023-03-27 12:04:02 +0300

    Model

--- HW3.4Quotes/Models/Quotes.swift
+++ HW3.4Quotes/Models/Quotes.swift
@@ -5,4 +5,25 @@
 //  Created by Максим Гурков on 27.03.2023.
 //
 
-import Foundation
+struct Quotes: Decodable {
+    
+    let quote: String
+    let character: String
+    let image: String
+    
+    init(quotesData: [String: Any]) {
+        quote = quotesData["quote"] as? String ?? ""
+        character = quotesData["character"] as? String ?? ""
+        image = quotesData["image"] as? String ?? ""
+    }
+    
+    static func getQuotes(from value: Any) -> [Quotes] {
+        guard let quotesData = value as? [[String: Any]] else { return [] }
+        return quotesData.map { Quotes(quotesData: $0) }
+        
+    }
+}
+
+enum Link: String {
+    case url = "https://thesimpsonsquoteapi.glitch.me/quotes"
+}
